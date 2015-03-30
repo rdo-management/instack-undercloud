c08a78040b891776f72ea3682560100fc2d55ae9
Brad P. Crochet <brad@redhat.com>
2015-03-30 15:51:15 -0400

    Enable tuskar installed and configured via puppet
    Change-Id: I98e484453ef9915fc8ff7586efc2d04170ba440f

--- elements/puppet-stack-config/puppet-stack-config.pp
+++ elements/puppet-stack-config/puppet-stack-config.pp
@@ -81,11 +81,19 @@ class { 'ceilometer::db::mysql':
 }
 $ironic_dsn = split(hiera('ironic::database_connection'), '[@:/?]')
 class { 'ironic::db::mysql':
-user          => $ironic_dsn[3],
-password      => $ironic_dsn[4],
-host          => $ironic_dsn[5],
-dbname        => $ironic_dsn[6],
-allowed_hosts => $allowed_hosts,
+  user          => $ironic_dsn[3],
+  password      => $ironic_dsn[4],
+  host          => $ironic_dsn[5],
+  dbname        => $ironic_dsn[6],
+  allowed_hosts => $allowed_hosts,
+}
+$tuskar_dsn = split(hiera('tuskar::database_connection'), '[@:/?]')
+class {'tuskar::db::mysql':
+  user          => $tuskar_dsn[3],
+  password      => $tuskar_dsn[4],
+  host          => $tuskar_dsn[5],
+  dbname        => $tuskar_dsn[6],
+  allowed_hosts => $allowed_hosts,
 }
 
 if $::osfamily == 'RedHat' {
@@ -341,6 +349,12 @@ package{'python-tuskarclient': }
 
 class { 'tuskar::ui':
   extras => true
+}
+
+include ::tuskar
+
+class { 'tuskar::api':
+  identity_uri => join(['http://', hiera('controller_host'), ':5000/v2.0']),
 }
 
 # tempest
