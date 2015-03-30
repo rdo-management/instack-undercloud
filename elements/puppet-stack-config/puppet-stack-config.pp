ae34afd52fdd0c87c4fe9fd9b109eeeb5f7d9f37
Brad P. Crochet <brad@redhat.com>
2015-03-30 15:51:15 -0400

    Enable tuskar installed and configured via puppet
    Change-Id: I98e484453ef9915fc8ff7586efc2d04170ba440f

--- elements/puppet-stack-config/puppet-stack-config.pp
+++ elements/puppet-stack-config/puppet-stack-config.pp
@@ -81,11 +81,19 @@ class { 'ceilometer::db::mysql':
 }
 $ironic_dsn = split(hiera('ironic::database_connection'), '[@:/?]')
 class { 'ironic::db::mysql':
-user          => $ironic_dsn[3],
-password      => $ironic_dsn[4],
-host          => $ironic_dsn[5],
-dbname        => $ironic_dsn[6],
-allowed_hosts => $allowed_hosts,
+  user          => $ironic_dsn[3],
+  password      => $ironic_dsn[4],
+  host          => $ironic_dsn[5],
+  dbname        => $ironic_dsn[6],
+  allowed_hosts => $allowed_hosts,
+}
+$tuskar_dsn = split(hiera('tuskar::database_connection'), '[@:/?]')
+class {'tuskar::db::mysql':
+  user          => $tuskar_dsn[3],
+  password      => $tuskar_dsn[4],
+  host          => $tuskar_dsn[5],
+  dbname        => $tuskar_dsn[6],
+  allowed_hosts => $allowed_hosts,
 }
 
 if $::osfamily == 'RedHat' {
@@ -350,6 +358,12 @@ class { 'horizon':
   server_aliases => [hiera('controller_host'), $::fqdn, 'localhost'],
 }
 
+include ::tuskar
+
+class { 'tuskar::api':
+  identity_uri => join(['http://', hiera('controller_host'), ':5000/v2.0']),
+}
+
 # Install python-tuskarclient so we can deploy a stack with tuskar
 package{'python-tuskarclient': }
 
@@ -360,3 +374,4 @@ class { 'tuskar::ui':
 # tempest
 # TODO: when puppet-tempest supports install by package, do that instead
 package{'openstack-tempest': }
+
