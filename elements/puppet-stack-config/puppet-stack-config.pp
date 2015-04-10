e1fd053b39a0131d2e3fe4d11eb83819596d79ed
Ben Nemec <bnemec@redhat.com>
2015-04-10 12:29:10 -0500

    WIP: Enable loadbalancer with ssl on undercloud
    Change-Id: Idd9492303e797f5b39c980772f33754fb44a9f12
    Depends-On: I6f87ed2ebbea08ff1a0dff981559c8f4fc8b67cc@https://review.openstack.org

--- elements/puppet-stack-config/puppet-stack-config.pp
+++ elements/puppet-stack-config/puppet-stack-config.pp
@@ -130,8 +130,17 @@ rabbitmq_user_permissions {[
 # pre-install swift here so we can build rings
 include ::swift
 
+if hiera('service_certificate', undef) {
+  $keystone_public_endpoint = join(['https://', hiera('controller_public_vip'), ':13000'])
+} else {
+  $keystone_public_endpoint = undef
+}
+
 class { 'keystone':
   debug => hiera('debug'),
+  public_bind_host => hiera('controller_host'),
+  admin_bind_host  => hiera('controller_host'),
+  public_endpoint  => $keystone_public_endpoint,
 }
 
 #TODO: need a cleanup-keystone-tokens.sh solution here
@@ -300,6 +309,7 @@ class { 'nova::compute::ironic':
   admin_user        => 'ironic',
   admin_passwd    => hiera('ironic::api::admin_password'),
   admin_tenant_name => hiera('ironic::api::admin_tenant_name'),
+  admin_url         => join(['http://', hiera('controller_host'), ':35357/v2.0']),
   api_endpoint      => join(['http://', hiera('controller_host'), ':6385/v1']),
 }
 
@@ -336,6 +346,27 @@ class { 'horizon':
 
 # Install python-tuskarclient so we can deploy a stack with tuskar
 package{'python-tuskarclient': }
+
+class { 'tripleo::loadbalancer':
+  controller_virtual_ip => hiera('controller_admin_vip'),
+  controller_hosts      => [hiera('controller_host')],
+  control_virtual_interface => 'br-ctlplane',
+  public_virtual_ip     => hiera('controller_public_vip'),
+  public_virtual_interface  => 'br-ctlplane',
+  service_certificate   => hiera('service_certificate', undef),
+  keystone_admin        => true,
+  keystone_public       => true,
+  neutron               => true,
+  cinder                => true,
+  glance_api            => true,
+  glance_registry       => true,
+  nova_osapi            => true,
+  nova_metadata         => true,
+  swift_proxy_server    => true,
+  heat_api              => true,
+  #horizon               => true,
+  rabbitmq              => true,
+}
 
 # tempest
 # TODO: when puppet-tempest supports install by package, do that instead
