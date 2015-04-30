45ff3900a57d3957fd26041d3e8831969e90c97f
Ben Nemec <bnemec@redhat.com>
2015-04-30 21:48:25 +0000

    WIP: Pythonize instack-install-undercloud
    Important points:
    -Very incomplete testing right now
    -Puts the password and stackrc files in the current user's home
     directory during the install.  Given that they now have secure
     permissions and we recommend doing that anyway, I think it's fine,
     but it is a non-trivial change in behavior.
    
     This was done because it's awkward to read a root-owned file from
     a Python process running as a regular user.
    -Uses oslo.config instead of the bash-style answers file, and much
     of the doc for the options is still missing.  A sample conf file
     generated with the oslo.config generator is included (for now,
     that may warrant more discussion).  Backwards compatibility with
     existing answers files is maintained for now, but is deprecated.
    
    Change-Id: I09270997dea7fdad2b40dfb303967ff425b55a9b

---
+++ instack_undercloud/tests/test_undercloud.py
@@ -0,0 +1,41 @@
+# Copyright 2015 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+from oslotest import base
+
+from instack_undercloud import undercloud
+
+# TODO(bnemec): Test all the things
+# TODO(bnemec): Stop the code from logging to the real log file during tests
+
+
+class TestUndercloud(base.BaseTestCase):
+    @mock.patch('instack_undercloud.undercloud._run_command')
+    @mock.patch('instack_undercloud.undercloud._configure_ssh_keys')
+    @mock.patch('instack_undercloud.undercloud._run_orc')
+    @mock.patch('instack_undercloud.undercloud._run_instack')
+    @mock.patch('instack_undercloud.undercloud._generate_environment')
+    @mock.patch('instack_undercloud.undercloud._load_config')
+    def test_install(self, mock_load_config, mock_generate_environment,
+                     mock_run_instack, mock_run_orc, mock_configure_ssh_keys,
+                     mock_run_command):
+        fake_env = mock.MagicMock()
+        mock_generate_environment.return_value = fake_env
+        undercloud.install('.')
+        mock_generate_environment.assert_called_with('.')
+        mock_run_instack.assert_called_with(fake_env)
+        mock_run_orc.assert_called_with(fake_env)
+        mock_run_command.assert_called_with(
+            ['sudo', 'rm', '-f', '/tmp/svc-map-services'], None, 'rm')
